fbbdf5349d3964c10e283f9f99c7d77614443b45
bibekyogi6462 <bibekyogi6462@gmail.com>
2025-10-11 11:58:11 +0545

    user model created

---
+++ backend/model/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.SchemaType(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["student", "instructor"],
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+      default: "",
+    },
+    enrolledCourses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;

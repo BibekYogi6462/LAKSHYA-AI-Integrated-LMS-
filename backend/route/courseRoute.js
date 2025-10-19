c9ccce94649bdc15912b4927eda6e5ea1648f175
bibekyogi6462 <bibekyogi6462@gmail.com>
2025-10-19 12:19:02 +0545

    courses route created

---
+++ backend/route/courseRoute.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  createCourse,
+  editCourse,
+  getCoursesById,
+  getCreatorCourses,
+  getPublisedCourses,
+  removeCourse,
+} from "../controller/courseController.js";
+import isAuth from "../middleware/isAuth";
+import upload from "../middleware/multer";
+
+const courseRouter = express.Router();
+
+courseRouter.post("/create", isAuth, createCourse);
+courseRouter.get("/getpublised", getPublisedCourses);
+courseRouter.get("/getcreator", isAuth, getCreatorCourses);
+
+courseRouter.post(
+  "/editcourse/:courseId",
+  isAuth,
+  upload.single("thumbnail"),
+  editCourse
+);
+
+courseRouter.get("/getcourse/:courseId", isAuth, getCoursesById);
+
+courseRouter.delete("/remove/:courseId", isAuth, removeCourse);
